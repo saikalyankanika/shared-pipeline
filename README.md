72d68b200bde7429963962c6a72e2786a070fc92
kanikasaikalyan <kanikasaikalyan@gmail.com>
2025-01-27 09:03:28 +0530

    commiting dependency check

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # shared-pipeline
 
+### prerequisites to use this library :
+- adding the library in global system settings in jenkins 
+- docker destop needed to be installed on your machine 
+
+
+
+
+### stages that the pipeline undergoes:
+
+```
+CI-Part:
+    - Initialization stage
+    - OSWAP dependencies pull
+    - sonar check
+    - build and push
+    - image build and push
+    - image scanning
+CD-Part:
+    - predeployment checks
+    - deployment 
+
+```
--- vars/init.groovy
+++ vars/init.groovy
@@ -1,5 +1,14 @@
 
-def call(){
+def call(string repourl){
+
+    
+    stage('OWSAP-Dependency-check'){
+
+        //checking out the cod e for dependency check
+        checkout changelog: false, poll: false, scm: scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/saikalyankanika/react-sample-ksk']])
+        //running the dependency check
+        dependencyCheck additionalArguments: '''--format HTML''', odcInstallation: 'DP-check', stopBuild: true
+    }
 
     println("Hello from init.groovy")
     if(env.sonar_scan!=null){
