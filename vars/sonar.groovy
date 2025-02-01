ee4c26151b8f2fc80d6988986d89f3c3854d1f26
kanikasaikalyan <kanikasaikalyan@gmail.com>
2025-02-02 02:28:21 +0530

    adding sonarcli

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -9,12 +9,12 @@ def call(String repourl){
                             container('sonar-cli') {
                                 //checking out the code for sonar scan inside /usr/src folder
                                 // dir('/usr/src/') {
-                                sh "git clone ${repourl} repo"
+                                sh "git clone ${repourl} /usr/src/repo"
                                 // checkout changelog: false, poll: false, scm: scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: "${repourl}"]])
 
                             //running the sonar scan
                                 withCredentials([string(credentialsId: 'sonar_key', variable: 'sonar')]) {
-                                    sh "cd repo/ && sonar-scanner -Dsonar.login=${env.sonar}"
+                                    sh "cd /usr/src/repo/ && sonar-scanner -Dsonar.login=${env.sonar}"
 
                                     }
                                         
