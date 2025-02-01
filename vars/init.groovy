4b278f99f75c0ced1c66488530a1f6fb52db31d1
kanikasaikalyan <kanikasaikalyan@gmail.com>
2025-02-02 01:16:15 +0530

    adding sonarcli

--- vars/init.groovy
+++ vars/init.groovy
@@ -1,21 +1,20 @@
 
 def call(String repourl){
 
-    
+    // OSWAP Dependency Check
     stage('OWSAP-Dependency-check'){
 
         //checking out the cod e for dependency check
-        checkout changelog: false, poll: false, scm: scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/saikalyankanika/react-sample-ksk']])
+        checkout changelog: false, poll: false, scm: scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: "${repourl}"]])
         //running the dependency check
         dependencyCheck additionalArguments: '''--format HTML''', odcInstallation: 'DP-check', stopBuild: true
     }
 
-    println("Hello from init.groovy")
+    // Sonar Scan
     if(env.sonar_scan!=null){
-    stage('Sonar Scan') {
-
-
-}
+        stage('Sonar Scan') {
+            sonar(repourl)
+        }
 
 }
 
---
+++ vars/sonar.groovy
@@ -0,0 +1,22 @@
+def call(String repourl){
+
+    podTemplate(containers: [
+                        containerTemplate(name: 'sonar-cli', image: 'sonarsource/sonar-scanner-cli', ttyEnabled: true, 
+                                          resourceRequestMemory: '64Mi', resourceRequestCpu: '250m', 
+                                          resourceLimitMemory: '128Mi', resourceLimitCpu: '500m')
+                    ]) {
+                        node(POD_LABEL) {
+                            container('sonar-cli') {
+                                //checking out the code for sonar scan
+                                checkout changelog: false, poll: false, scm: scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: "${repourl}"]])
+
+                                //running the sonar scan
+                                withCredentials([string(credentialsId: 'sonar_key', variable: 'sonar')]) {
+                                    sh "sonar-scanner -Dsonar.login=${sonar}"
+                                    // some block
+                                }
+
+                            }
+                        }
+}
+}
